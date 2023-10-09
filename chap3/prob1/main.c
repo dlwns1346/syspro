ac9e55d6fab011ca20390a3c959a167953df7d2c
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-10-09 16:55:37 +0900

    Update main.c

--- chap3/prob1/main.c
+++ chap3/prob1/main.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
-#include <string.h>
-#include "copy.h"
-char line[5][MAXLINE]; // 입력 줄
-char longest[5][MAXLINE]; // 가장 긴 줄
-void copy(char from[], char to[]);
+#include "copy.h“
 
+char line[MAXLINE]; // 입력 줄
+char longest[MAXLINE]; // 가장 긴 줄
+/* 입력 줄 가운데 가장 긴 줄을 프린트한다. */
 
-/*입력 줄 가운데 가장 긴 줄 프린트 */
-int main()
-{
-   int len, max = 0;
+main(){
+  int len;
+  int max;
+  max = 0;
 
-   int i = 0;
+  while(gets(line) != NULL) {
+    len = strlen(line);
 
-   while (i < 5) {
-      fgets(line[i],MAXLINE,stdin);
-      len = strlen(line[i]);
-      if (len > max) {
-         max = len;
-         copy(line[i], longest[max]);
-      } else {
-         copy(line[i], longest[len]);
+    if(len > max) {
+       max = len;
+       copy(line, longest);
       }
-      i++;
-   }
+    }
 
-   while (max > 0) {// 입력 줄이 있었다면
-      printf("%s", longest[max]);
-      max--;
-   }
+  if(max > 0) // 입력 줄이 있었다면
+    printf("%s \n", longest);
 
-   return 0;
+  return 0;
 }
