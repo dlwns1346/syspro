5c3630abd8e7ff673a6078c5797f8b04695dee8c
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-10-18 23:25:21 +0900

    Update copy.c

--- chap4/prob2/copy.c
+++ chap4/prob2/copy.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
    char c;
    FILE *fp1, *fp2;
    if (argc !=3) {
-      fprintf(stderr,  “How to use: %s File1 File2\n", argv[0]);
+      fprintf(stderr,  "How to use: %s File1 File2\n", argv[0]);
       return 1;
    }
    
