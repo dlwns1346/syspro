bd899f85c7dc6a27ed848d46c968c0b238b75f54
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-10-18 23:38:29 +0900

    Update stprint.c

--- chap4/prob7/stprint.c
+++ chap4/prob7/stprint.c
@@ -6,16 +6,16 @@ int main(int argc, char* argv[])
    struct student rec;
    FILE *fp;
    if (argc != 2) { 
-      fprintf(stderr, “How to use: %s FileName\n", argv[0]);
+      fprintf(stderr, "How to use: %s FileName\n", argv[0]);
       return 1; 
    }
    if ((fp = fopen(argv[1], "rb")) == NULL ) {
-      fprintf(stderr, “Error Open File\n");
+      fprintf(stderr, "Error Open File\n");
       return 2;
    }
 
    printf("-----------------------------------\n");
-   printf("%10s %6s %6s\n", “StudentID", “Name", “Score");
+   printf("%10s %6s %6s\n", "StudentID", "Name", "Score");
    printf("-----------------------------------\n");
    
    while (fread(&rec, sizeof(rec), 1, fp) > 0) 
