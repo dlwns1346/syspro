f07ca3c679596d4ec78879403c27e636009c0aa4
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-10-20 17:10:15 +0900

    Update fsize.c

--- chap5/prob2/fsize.c
+++ chap5/prob2/fsize.c
@@ -11,3 +11,15 @@ int main(int argc, char *argv[])
    int fd;
    ssize_t nread;
    long total = 0;
+
+   if ((fd = open(argv[1], O_RDONLY)) == -1) 
+      perror(argv[1]);
+
+   /* 파일의 끝에 도달할 때까지 반복해서 읽으면서 파일 크기 계산 */
+   while( (nread = read(fd, buffer,    BUFSIZE)) > 0)
+      total += nread;
+
+   close(fd);
+   printf ("%s File size : %ld Byte \n", argv[1], total);
+   exit(0);
+}
