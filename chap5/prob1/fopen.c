d9b418b2dfe123aea2a4366e1cfd2a93f31351aa
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-10-23 01:59:12 +0900

    Update fopen.c

--- chap5/prob1/fopen.c
+++ chap5/prob1/fopen.c
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
    int fd;
 
    if ((fd = open(argv[1], O_RDWR)) == -1)
-      printf(“File Open Error\n");
-   else printf(“file %s Open Sucess : %d\n", argv[1], fd);
+      printf("File Open Error\n");
+   else printf("file %s Open Sucess : %d\n", argv[1], fd);
 
    close(fd);
    exit(0);
