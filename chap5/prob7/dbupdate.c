bc06a86a1a3f5994403647a808fc0e71ec1af198
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-10-27 03:00:53 +0900

    Add files via upload

--- chap5/prob7/dbupdate.c
+++ chap5/prob7/dbupdate.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
    char c;
    struct student record;
    if (argc < 2) {
-      fprintf(stderr, “How to use : %s file\n", argv[0]);
+      fprintf(stderr, "How to use : %s file\n", argv[0]);
       exit(1);
    }
    if ((fd = open(argv[1], O_RDWR)) == -1) {
@@ -19,19 +19,19 @@ int main(int argc, char *argv[])
    }
 
 do {
-      printf(“Enter StudentID to be modified: ");
+      printf("Enter StudentID to be modified: ");
       if (scanf("%d", &id) == 1) {
          lseek(fd, (long) (id-START_ID)*sizeof(record), SEEK_SET);
          if ((read(fd, (char *) &record, sizeof(record)) > 0) && (record.id != 0)) {
-            printf(“StuID:%8d\t Name:%4s\t Score:%4d\n",
+            printf("StuID:%8d\t Name:%4s\t Score:%4d\n",
                     record.id, record.name, record.score);
-            printf(“Enter New Score: ");
+            printf("Enter New Score: ");
             scanf("%d", &record.score);
             lseek(fd, (long) -sizeof(record), SEEK_CUR);
             write(fd, (char *) &record, sizeof(record));
-         } else printf(“Record %d Null\n", id);
-      } else printf(“Insert Error\n");
-      printf(“Continue?(Y/N)");
+         } else printf("Record %d Null\n", id);
+      } else printf("Insert Error\n");
+      printf("Continue?(Y/N)");
       scanf(" %c",&c);
    } while (c == 'Y');
    close(fd);
