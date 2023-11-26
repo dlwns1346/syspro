ebce3dfec8e820d4c25b411240192e5c0db5b166
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-11-26 18:15:48 +0900

    Update waitpid.c

--- chap9/prob2/waitpid.c
+++ chap9/prob2/waitpid.c
@@ -20,13 +20,13 @@
 	
 	 pid2 = fork();
 	 if (pid2 == 0) {
-	     printf("[%d] 자식 프로세스 #2 시작 \n", getpid( ));
+	     printf("[%d] 자식 프로세스[2] 시작 \n", getpid( ));
 	     sleep(2);
-	     printf("[%d] 자식 프로세스 #2 종료 \n", getpid( ));
+	     printf("[%d] 자식 프로세스[2] 종료 \n", getpid( ));
 	     exit(2);
 	 }
 	 // 자식 프로세스 #1의 종료를 기다린다.
 	 child = waitpid(pid1, &status, 0); 
-	 printf("[%d] 자식 프로세스 #1 %d 종료 \n", getpid( ), child);
+	 printf("[%d] 자식 프로세스[1] %d 종료 \n", getpid( ), child);
 	 printf("\t종료 코드 %d\n", status>>8);
 	}
