9500782c451ba8e7f7cf11ad22cd9d0cb16a5d69
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-12-09 00:48:21 +0900

    Create sigint2.c

---
+++ chap11/prob4/sigint2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+struct sigaction newact;
+struct sigaction oldact;
+void sigint_handler(int signo); 
+
+int main( void)
+{
+     newact.sa_handler = sigint_handler; // 시그널 처리기 지정
+     sigfillset(&newact.sa_mask); // 모든 시그널을 차단하도록 마스크 
+ 
+    // SIGINT의 처리 액션을 새로 지정, oldact에 기존 처리 액션을 저장
+    sigaction(SIGINT, &newact, &oldact);
