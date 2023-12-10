2633a5bb985fe65bf934a53b1f230274495d8357
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-12-11 01:00:31 +0900

    Add files via upload

--- chap11/prob2/almhandler.c
+++ chap11/prob2/almhandler.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 void alarmHandler();
 /* 알람 시그널을 처리한다. */
 int main( )
 {
+   int n=1;
    signal(SIGALRM, alarmHandler);
    alarm(5);    /* 알람 시간 설정 */
-   printf("무한 루프 \n");
    while (1) {
       sleep(1);
-      printf("1초 경과 \n");
+      printf("%d second \n", n++);
    }
    printf("실행되지 않음 \n");
 }
 void alarmHandler(int signo)
 {
-   printf("일어나세요\n");
+   printf("Wake up\n");
    exit(0);
 }
