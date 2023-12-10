4702bdf6baf88681d0d3f09de878c8d3422878c3
dlwns1346 <122613633+dlwns1346@users.noreply.github.com>
2023-12-11 01:01:31 +0900

    Add files via upload

--- chap11/prob6/control.c
+++ chap11/prob6/control.c
@@ -12,6 +12,27 @@ int main( )
    if (pid1 == 0) {
       while (1) {
          sleep(1);
-         printf(“자식 [1] 실행: %d\n, ++count1");
+         printf("child [1] execute: %d\n", ++count1);
       }
    }
+
+   pid2 = fork( );
+   if (pid2 == 0) {
+      while (1) {
+         sleep(1);
+         printf("child [2] execute: %d\n", ++count2);
+      }
+   }
+   sleep(2);
+   kill(pid1, SIGSTOP);
+   sleep(2);
+   kill(pid1, SIGCONT);
+   sleep(2);
+   kill(pid2, SIGSTOP);
+   sleep(2);
+   kill(pid2, SIGCONT);
+   sleep(2);
+   kill(pid1, SIGKILL);
+   kill(pid2, SIGKILL);
+}
+
